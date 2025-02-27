19354467ada7d2eec8e616242aca6b78ef1e517f
Tusharbendre01 <tusharbendre01@gmail.com>
2025-02-27 13:41:03 +0530

    PortfolioControllerTesting Done

--- wms-springboot-main/src/test/java/com/cognizant/wms/controllertesting/PortfolioControllerTest.java
+++ wms-springboot-main/src/test/java/com/cognizant/wms/controllertesting/PortfolioControllerTest.java
@@ -12,7 +12,6 @@ import org.mockito.MockitoAnnotations;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.mockito.Mockito.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
